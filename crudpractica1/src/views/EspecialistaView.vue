300c9f915443c488e72d6ebab68c9ed8816d8d0f
alexandraflorez1 <alexandra.florez1@udea.edu.co>
2024-10-06 19:38:48 -0500

    segundo commit: Visualización por especialista

--- crudpractica1/src/views/EspecialistaView.vue
+++ crudpractica1/src/views/EspecialistaView.vue
@@ -1,16 +1,27 @@
 <template>
+  <div class="container mt-5">
     <div id="app">
       <h1>BIENVENIDO AL SISTEMA DE ESPECIALISTA</h1>
-      <label for="clave">Ingrese la clave predeterminada:  </label>
+      <label for="clave">Ingrese la clave  </label>
       <input
         type="password"
         v-model="clave"
         placeholder="Clave"
         id="clave"
       />
-      <button @click="handleSubmit">OK</button>
-      <p v-if="mensaje">{{ mensaje }}</p>
     </div>
+
+    <button
+      class="btn btn-primary"
+      @click="verificarCodigo"
+    >
+      OK
+    </button>
+
+    <div v-if="mensaje" class="mt-3 alert">
+      {{ mensaje }}
+    </div>
+  </div>
   </template>
   
   <script>
@@ -23,44 +34,15 @@
       };
     },
     methods: {
-      handleSubmit() {
-        if (this.clave === this.claveCorrecta) {
-          this.mensaje = 'Clave ingresada correctamente.';
-        } else {
-          this.mensaje = 'Clave incorrecta. Inténtalo de nuevo.';
-        }
-      },
-    },
-  };
+  verificarCodigo() { 
+    if (this.clave === this.claveCorrecta) {
+      this.$router.push('/listar');
+    } else {
+      this.mensaje = 'Clave incorrecta. Inténtalo de nuevo.';
+      this.clave = ''; // Limpia el campo de entrada cuando la clave es incorrecta
+    }
+  },
+},
+};
   </script>
-  
-  <style>
-  /* Estilos opcionales */
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  
-  input {
-    padding: 10px;
-    font-size: 16px;
-    width: 200px;
-    margin-bottom: 20px;
-  }
-  
-  button {
-    padding: 10px 15px;
-    font-size: 16px;
-    background-color: #42b983;
-    color: white;
-    border: none;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #35495e;
-  }
-  </style>
   
--- crudpractica1/src/views/ListarView.vue
+++ crudpractica1/src/views/ListarView.vue
@@ -1,11 +1,146 @@
 <template>
-    <h1>Para listar pacientes</h1>
+
+  <div class="container"> 
+    <div class="input-group mb-3">
+    <span class="input-group-text"  id="inputGroup-sizing-default">Documento </span>
+    <input v-model= "docBuscar" type="text" placeholder="Ingrese el documento" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+    <button 
+    class="btn btn-primary" 
+    type="button"
+    id="inputGroup-sizing-default"
+    @click="BuscarPaciente(docBuscar)"
+    >Buscar</button>
+  </div>
+    
+    <div class="card"> 
+      <div class="card-header">
+        <h1>LISTA DE PACIENTES</h1>
+      </div>
+      <div class="card-body">
+        <div
+          class="table-responsive"
+        >
+          <table
+            class="table table-primary"
+          >
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Documento</th>
+                <th scope="col">Nombres</th>
+                <th scope="col">Apellidos</th>
+                <th scope="col">   </th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr  v-for="paciente in Pacientes" v-bind:key="paciente.id">
+                  <td scope="row">{{ paciente.id }}</td>
+                  <td>{{ paciente.Doc }}</td>
+                  <td>{{ paciente.Nombre }}</td>        
+                  <td>{{ paciente.Apellido }}</td>   
+                  <td>
+                    <div class="btn-group" role="group" aria-label="Button group name">
+                      <button
+                        type="button"
+                        class="btn btn-primary"
+                      >
+                        Ver resultados
+                      </button>
+                     
+                      <router-link :to="{ name: 'editar', params: { id: paciente.id } }" class="btn btn-primary">Editar</router-link>
+
+                      <button
+                        type="button"
+                        class="btn btn-danger"
+                        @click="eliminarPaciente(paciente.id)"
+
+                      >
+                        Eliminar
+                      </button>
+                    </div>
+                    
+                  </td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        
+      
+      </div>
+      
+    </div>
+  </div>
   </template>
   
   <script>
 
 export default {
+  data(){
+    return{ 
+    Pacientes:[],
+    pacienteEncontrado:[], // Para almacenar los datos del paciente si se encuentra
+    mensajeError: '', // Mensaje de error en caso de fallo o paciente no encontrado
+  }
+  },
   
+
+    created:function(){
+      this.consultarPacientes(); 
+    },
+
+    methods:{
+      consultarPacientes(){
+         fetch('http://localhost/API/')
+        .then(response => response.json())
+        .then((data)=>{
+          console.log(data)
+          this.Pacientes=[];
+          if(typeof data[0].sucess==='undefined'){
+          this.Pacientes=data
+        }
+        
+        })
+        .catch(console.log)
+      },
+
+      eliminarPaciente(id){
+        console.log(id);
+        fetch('http://localhost/API/?borrar='+id)
+          .then(response=>response.json())
+          .then((data)=>{
+            console.log(data)
+            window.location.href='listar'
+        })
+        .catch(console.log)
+      },
+
+    BuscarPaciente(doc) {
+      console.log(doc);
+      fetch('http://localhost/API/?consultar='+doc) //se hace uso de la API para consultar un paciente por numero de documento
+      .then(response=>response.json())
+      .then((data) => {
+        if(typeof data[0].sucess==='undefined'){
+          this.pacienteEncontrado=data
+          console.log(data);
+        }
+        
+        else {
+          console.log('Paciente no encontrado');
+          this.mensajeError = 'Paciente no encontrado';
+          this.pacienteEncontrado = null; // Reinicia los datos si no se encuentra el paciente
+        }
+        })
+
+        .catch((error) => {
+        // Manejo de errores
+        console.log('Error al buscar el paciente:', error);
+        this.mensajeError = 'Error al buscar el paciente.';
+      });
+  }
+},
+
 }
 </script>
   
